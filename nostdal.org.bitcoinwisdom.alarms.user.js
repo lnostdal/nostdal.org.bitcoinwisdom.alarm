03a2df8ad54ffa28ba229726e46d452fa48d06f6
Lars Rune Nøstdal <larsnostdal@gmail.com>
2014-02-16 18:04:31 +0100

    Initial userscript added. Tested with Chrome.

---
+++ nostdal.org.bitcoinwisdom.alarms.user.js
@@ -0,0 +1,88 @@
+// ==UserScript==
+// @name Bitcoinwisdom price alarms
+// @match http://bitcoinwisdom.com/*
+// ==/UserScript==
+
+
+var main = function(){
+
+  var beep = (function () {
+    var ctx = new(window.audioContext || window.webkitAudioContext);
+    return function (duration, freq, type){
+      duration = +duration;
+
+      type = (type % 5) || 0;
+
+      var osc = ctx.createOscillator();
+
+      osc.type = type;
+      osc.frequency.value = freq;
+
+      osc.connect(ctx.destination);
+      osc.noteOn(0);
+
+      setTimeout(function (){
+        osc.noteOff(0);
+      }, duration);
+    };
+  })();
+
+
+
+  (function($) {
+    $.qs = (function(a) {
+      if (a == "") return {};
+      var b = {};
+      for (var i = 0; i < a.length; ++i)
+      {
+        var p=a[i].split('=');
+        if (p.length != 2) continue;
+        b[p[0]] = decodeURIComponent(p[1].replace(/\+/g, " "));
+      }
+      return b;
+    })(window.location.search.substr(1).split('&'));
+  })(jQuery);
+
+
+  function addAlarmsToURL(){
+    window.history.replaceState(null, '', '?low_alarm=' + $("#low_alarm").val() + "&high_alarm=" + $("#high_alarm").val());
+  }
+
+
+
+  $("#periods").append("<li class='sep'></li> <li>Low alarm: <input id='low_alarm' type='text' style='width: 4em;'></li> <li>High alarm: <input id='high_alarm' type='text' style='width: 4em;'></li>");
+
+
+
+  // On page (re)load fetch values from URL.
+  $("#low_alarm").val($.qs["low_alarm"]);
+  $("#high_alarm").val($.qs["high_alarm"]);
+
+
+
+
+  var low_alarm = window.setInterval(function(){
+    if(parseFloat($("#price").html()) <= parseFloat($("#low_alarm").val())){
+      beep(500, 440, 0);
+    }
+    addAlarmsToURL();
+  },
+                                     1000);
+
+
+
+  var high_alarm = window.setInterval(function(){
+    if(parseFloat($("#price").html()) >= parseFloat($("#high_alarm").val())){
+      beep(500, 880, 0);
+    }
+  },
+                                      1000);
+
+};
+
+
+
+var script = document.createElement('script');
+script.type = "text/javascript";
+script.textContent = '(' + main.toString() + ')();';
+document.body.appendChild(script);
